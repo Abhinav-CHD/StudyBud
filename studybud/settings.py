bee91bb187f61a5a1c7abfb439dd207b3731ebe8
Abhinav-CHD <abhinavsharmajuly@gmail.com>
2022-11-22 02:11:16 +0530

    changed debug to true and fixed favicon

--- studybud/settings.py
+++ studybud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-713d.up.railway.app','studybud-u3jj.onrender.com']
+ALLOWED_HOSTS = ['web-production-713d.up.railway.app','studybud-u3jj.onrender.com','127.0.0.1']
 
 
 # Application definition
